e563f8c748ae523639fce171751249f7525bfd32
ted-han <taekyung28h@gmail.com>
2020-01-06 08:11:51 -0500

    Add react-router-dom, styled-components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import DonationLIst from './components/DonationList';
+import DetailInfo from './components/DetailInfo';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React TK
-        </a>
-      </header>
+      <Route path="/" exact={true} component={DonationLIst} />
+      {/* <Route path="/:year" component={DonationLIst} /> */}
+      <Route path="/detail" exact={true} component={DetailInfo} />
+      <Route path="/detail/:name" component={DetailInfo} />
     </div>
   );
-}
+};
 
 export default App;
