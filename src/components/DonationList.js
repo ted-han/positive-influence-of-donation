91050dab4edaf98766066b7114701b289674f6db
ted-han <taekyung28h@gmail.com>
2020-02-19 19:51:43 -0500

    Add infinity scroll

--- src/components/DonationList.js
+++ src/components/DonationList.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import DonationItem from './DonationItem';
 import styled from 'styled-components';
 import db from '../firebase';
+import { getLastMonth } from '../myModule';
 
 const MainBlock = styled.div`
   display: flex;
@@ -19,29 +20,19 @@ const DonationListBlock = styled.div`
 class DonationList extends Component {
   state = {
     donations: null,
-    year: this.props.match.params.year || 'all'
+    endAtDate: null,
+    loading: false
   };
 
   loadData = async () => {
     try {
       console.log(`############로드데이터########`);
-      const year = this.state.year;
-
-      let resDonationData;
-      let donationRef = db.collection('donation');
-      if (year === 'all') {
-        // resDonationData = await donationRef.orderBy('date', 'desc').get();
-        resDonationData = await donationRef
-          .where('date', '>=', 20190101)
-          .orderBy('date', 'desc')
-          .get();
-      } else {
-        resDonationData = await donationRef
-          .where('date', '>=', `${year}0101`)
-          .where('date', '<=', `${year}1231`)
-          .orderBy('date', 'desc')
-          .get();
-      }
+      let endAt = getLastMonth().to;
+      let resDonationData = await db
+        .collection('donation')
+        .orderBy('date', 'desc')
+        .endAt(endAt)
+        .get();
 
       this.setState({
         donations: resDonationData.docs.map(doc => {
@@ -50,7 +41,8 @@ class DonationList extends Component {
             ...doc.data(),
             id: doc.id
           };
-        })
+        }),
+        endAtDate: endAt
       });
     } catch (error) {
       console.log(error);
@@ -59,21 +51,74 @@ class DonationList extends Component {
 
   componentDidMount() {
     this.loadData();
+    window.addEventListener('scroll', this.handleScroll, true);
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    // console.log(nextProps);
+    // console.log(nextState);
+    if (nextState.loading) return false;
+    return true;
+  }
+
+  handleScroll = () => {
+    const scrollHeight = document.getElementById('root').scrollHeight;
+    const clientHeight = document.getElementById('root').clientHeight;
+    const scrollY = window.scrollY;
+
+    // setScroll(scrollY);
+    if (scrollHeight - clientHeight - scrollY <= 400 && !this.state.loading) {
+      console.log('인피니티');
+      this.setState(
+        {
+          loading: true
+        },
+        async () => {
+          try {
+            let fromToDate = getLastMonth(this.state.endAtDate);
+
+            let resDonationData = await db
+              .collection('donation')
+              .orderBy('date', 'desc')
+              .startAfter(fromToDate.from)
+              .endAt(fromToDate.to)
+              .get();
+            let addData = resDonationData.docs.map(doc => {
+              return {
+                ...doc.data(),
+                id: doc.id
+              };
+            });
+
+            this.setState({
+              donations: this.state.donations.concat(addData),
+              endAtDate: fromToDate.to
+            });
+          } catch (error) {
+            console.log(error);
+          }
+          this.setState({
+            loading: false
+          });
+        }
+      );
+    }
+  };
+
   render() {
+    console.log('렌더');
     const { donations } = this.state;
     if (!donations) {
       return (
         <MainBlock>
           <Header category={'donation'} />
-          <div>aaaa</div>
+          <div></div>
         </MainBlock>
       );
     }
 
     return (
-      <MainBlock>
+      <MainBlock onScroll={this.handleScroll}>
         <Header category={'donation'} />
         <DonationListBlock>
           {donations.map(value => {
